8528c83fbc8e2af1439d21f452e0931397cb2603
hltro <lindjjye@gmail.com>
2023-11-14 11:36:02 -0500

    adjusted design and color for light/dark modes

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
-import {
-  Box,
-  List,
-  ListItem,
-  ListIcon,
-  Divider,
-  Text,
-  useColorModeValue
-} from '@chakra-ui/react';
-import { AiOutlineMessage } from 'react-icons/ai';
+import { Box, Flex, Text, useColorModeValue, VStack } from '@chakra-ui/react';
 
-// Mock data as placeholders to display chat messages
-const chats = [
-  { id: 1, title: 'Lorem ipsum', summary: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', time: 'Just now' },
-  { id: 2, title: 'Lorem ipsum', summary: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', time: '2 Min ago' },
-  { id: 3, title: 'Lorem ipsum', summary: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', time: '1 Day ago' },
+// Mock data to display messages
+const messages = [
+  { id: 1, text: 'How do I make an HTTP request in Javascript?', isUser: true },
+  { id: 2, text: 'You can use the fetch API or XMLHttpRequest for making HTTP requests.', isUser: false },
+  { id: 1, text: 'How do I make an HTTP request in Javascript?', isUser: true },
+  { id: 2, text: 'You can use the fetch API or XMLHttpRequest for making HTTP requests.', isUser: false },
+  { id: 1, text: 'How do I make an HTTP request in Javascript?', isUser: true },
+  { id: 2, text: 'You can use the fetch API or XMLHttpRequest for making HTTP requests.', isUser: false },
 ];
 
 function ChatList() {
-  const chatBgColor = useColorModeValue('gray.100', 'gray.700');
-  const chatTextColor = useColorModeValue('gray.600', 'gray.200');
-  const iconSize = '24px'; 
-  const textSize = 'md'; 
-  const summarySize = 'sm'; 
-  const timeSize = 'sm'; 
+  const bgColor = useColorModeValue('gray.100', 'gray.800'); // Darker background for dark mode
+  const userMessageColor = useColorModeValue('blue.600', 'blue.300'); // Lighter blue for dark mode
+  const appMessageColor = useColorModeValue('gray.200', 'gray.600'); // Darker gray for app messages in dark mode
+  const userTextColor = useColorModeValue('white', 'gray.50'); // White text for user in light mode
+  const appTextColor = useColorModeValue('gray.700', 'gray.50'); // Very light text for dark mode
 
   return (
-    <Box bg={chatBgColor} w="full" p={4} color={chatTextColor} overflowY="auto">
-      <List spacing={6}> 
-        {chats.map((chat) => (
-          <ListItem key={chat.id} p={3} >
-            <ListIcon>
-              <AiOutlineMessage size={iconSize} color="green.500" />
-            </ListIcon>
-            <Box pl={3}> 
-              <Text fontWeight="bold" fontSize={textSize}>{chat.title}</Text>
-              <Text fontSize={summarySize}>{chat.summary}</Text>
-              <Text fontSize={timeSize} color="gray.500">{chat.time}</Text>
-            </Box>
-            <Divider my={4} />
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <VStack
+      bg={bgColor}
+      p={4}
+      height="calc(100vh - 120px)"
+      overflowY="auto"
+      spacing={4}
+      align="stretch"
+    >
+      {messages.map((message) => (
+        <Flex
+          key={message.id}
+          justifyContent={message.isUser ? 'flex-end' : 'flex-start'}
+          w="full"
+        >
+          <Box
+            bg={message.isUser ? userMessageColor : appMessageColor}
+            color={message.isUser ? userTextColor : appTextColor} 
+            p={3}
+            borderRadius="lg"
+            maxW="70%"
+          >
+            <Text>{message.text}</Text>
+          </Box>
+        </Flex>
+      ))}
+    </VStack>
   );
 }
 
