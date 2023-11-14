0a660ba6249f19f1e8b4918560444a0cec1cee46
hltro <lindjjye@gmail.com>
2023-11-14 12:02:03 -0500

    design changes to sidebar: hover mode, count, etc.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,73 +3,72 @@ import {
   Box,
   VStack,
   Button,
+  Divider,
   useColorModeValue,
   Text,
-  Heading
+  Heading,
+  HStack,
+  Badge
 } from '@chakra-ui/react';
 import { BiConversation } from 'react-icons/bi';
 import { DiGoogleAnalytics } from 'react-icons/di';
-import { AiOutlineCalculator,AiTwotoneSetting } from 'react-icons/ai';
+import { AiOutlineCalculator, AiTwotoneSetting } from 'react-icons/ai';
 
 function Sidebar() {
   const sidebarBgColor = useColorModeValue('gray.100', 'gray.700');
   const sidebarTextColor = useColorModeValue('gray.600', 'gray.200');
+  const dividerColor = useColorModeValue('gray.300', 'gray.600');
+  const hoverBgColor = useColorModeValue('gray.200', 'gray.600');
 
-  const iconSize = '24px';
+  const iconSize = '28px';
+  const fontSize = '18px'; 
+  const badgeFontSize = '1em'; 
+
+  // Mock numbers for demonstration purposes
+  const pastChatsCount = 5;
+  const valuationModelsCount = 3;
 
   return (
     <Box
       bg={sidebarBgColor}
-      w={{ base: 'full', md: '250px' }} 
+      w={{ base: 'full', md: '300px' }}
       px={4}
       py={5}
       h="full"
       color={sidebarTextColor}
     >
       <VStack spacing={4} align="stretch">
-        <Heading size="xl" mb={2}> 
-          Valuation Chatbot
-        </Heading>
-        <Text fontSize="20px" color="#f0ab3c" fontWeight='bold'> 
-          CA Ventures
-        </Text>
+        <Heading size="2xl" mb={2}>Valuation Chatbot</Heading>
+        <Text fontSize="xl" color="#f0ab3c" fontWeight='bold'>CA Ventures</Text>
+        <Divider my={4} borderColor={dividerColor} />
         {/* Menu Items */}
-        <Button
-          variant="ghost"
-          leftIcon={<BiConversation size={iconSize} />}
-          justifyContent="flex-start"
-          iconSpacing={4}
-          size="lg" 
-        >
-          Past Chats
-        </Button>
-        <Button
-          variant="ghost"
-          leftIcon={<AiOutlineCalculator size={iconSize} />}
-          justifyContent="flex-start"
-          iconSpacing={4}
-          size="lg" 
-        >
-          Valuation Models
-        </Button>
-        <Button
-          variant="ghost"
-          leftIcon={<DiGoogleAnalytics size={iconSize} />}
-          justifyContent="flex-start"
-          iconSpacing={4}
-          size="lg" 
-        >
-          Analytics
-        </Button>
-        <Button
-          variant="ghost"
-          leftIcon={<AiTwotoneSetting size={iconSize} />}
-          justifyContent="flex-start"
-          iconSpacing={4}
-          size="lg" 
-        >
-          Setting
-        </Button>
+        {[
+          { icon: BiConversation, text: 'Past Chats', count: pastChatsCount },
+          { icon: AiOutlineCalculator, text: 'Valuation Models', count: valuationModelsCount },
+          { icon: DiGoogleAnalytics, text: 'Analytics' },
+          { icon: AiTwotoneSetting, text: 'Setting' },
+        ].map((item, index) => (
+          <Button
+            key={index}
+            variant="ghost"
+            justifyContent="flex-start"
+            iconSpacing={4}
+            size="lg"
+            fontSize={fontSize}
+            _hover={{ bg: hoverBgColor }}
+            w="full"
+            rightIcon={item.count ? (
+              <Badge colorScheme="green" ml={1} sx={{ fontSize: badgeFontSize }}>
+                {item.count}
+              </Badge>
+            ) : null}
+          >
+            <HStack>
+              <Box as={item.icon} size={iconSize} />
+              <Text flex={1}>{item.text}</Text>
+            </HStack>
+          </Button>
+        ))}
       </VStack>
     </Box>
   );
